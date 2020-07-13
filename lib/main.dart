a9976006d8d4a93fe9ac622533b915daa7ea46e1
cedricaxel <coboguie@gmail.com>
2020-07-13 19:04:40 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
 
-        home: SideBarLayou(),
+        home: Login(),
         routes: {
           "newTasks": (context) => NewTasks(),
           "ongoingTask": (context) => OngoingTask()
